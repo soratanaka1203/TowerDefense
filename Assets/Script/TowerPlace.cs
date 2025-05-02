e5ae3191abadfd133d732c954a16515eeb47a5ce
tanakasora1203 <sora1042hym@gmail.com>
2025-05-02 10:14:55 +0900

    ホバーしたときの演出の追加

--- Assets/Script/TowerPlace.cs
+++ Assets/Script/TowerPlace.cs
@@ -1,7 +1,33 @@
-
 using UnityEngine;
 
 public class TowerPlace : MonoBehaviour
 {
-    public bool isOccupied = false; // ‚·‚Å‚Éƒ^ƒ[‚ª’u‚©‚ê‚½‚©‚Ç‚¤‚©
+    public bool isOccupied = false;
+
+    public Material defaultMaterial;
+    public Material highlightMaterial;
+
+    private Renderer rend;
+
+    void Start()
+    {
+        rend = GetComponent<Renderer>();
+        rend.material = defaultMaterial;
+    }
+
+    void OnMouseEnter()
+    {
+        if (!isOccupied)
+        {
+            rend.material = highlightMaterial;
+        }
+    }
+
+    void OnMouseExit()
+    {
+        if (!isOccupied)
+        {
+            rend.material = defaultMaterial;
+        }
+    }
 }
